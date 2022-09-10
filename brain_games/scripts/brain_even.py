ed84bc27564017832e2176b9bfd68860de220ded
onthemod <onthemod@yandex.ru>
2022-09-10 13:05:25 +0300

    refactor brain_even.py to separate forming question

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,17 +2,27 @@ import prompt
 import random
 
 
-def main():
+def get_q_and_a():
+    num = random.randint(0, 100)
+    ans = 'yes' if num % 2 == 0 else 'no'
+    return (num, ans)
+
+
+def greet():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
+    return name
+
+
+def play_game():
+    name = greet()
     attempt = 0
     while attempt < 3:
-        num = random.randint(0, 100)
-        print(f'Question: {str(num)}')
+        qa = get_q_and_a()
+        ra = qa[0]
+        print(f'Question: {qa[0]}')
         ans = prompt.string('Your answer: ')
-        ra = 'yes'
-        if num % 2 == 1:
-            ra = 'no'
+        ra = qa[1]
         if ans != ra:
             print(f"\'{ans}\' is wrong answer ;(. Correct answer was \'{ra}\'.")
             print(f"Let\'s try again, {name}!")
@@ -20,6 +30,10 @@ def main():
         print('Correct!')
         attempt = attempt + 1
     print(f"Congratulations, {name}!")
+
+
+def main():
+    play_game()
 
 
 if __name__ == main:
