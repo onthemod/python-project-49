f2f05dc689b0d8ce7a3ba4e1ca8ba860ca541e83
onthemod <onthemod@yandex.ru>
2022-09-10 22:02:59 +0300

    added find greatest commod divisor game

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import random
+from brain_games.game_engine import play_game
+
+
+def get_q_and_a():
+    num1 = random.randint(0, 100)
+    num2 = random.randint(0, 100)
+    ans = 1
+    if num1 < num2:
+        for i in range(num1):
+            if num2 % (i + 1) == 0 and num1 % (i + 1) == 0:
+                ans = i + 1
+    else:
+        for i in range(num2):
+            if num2 % (i + 1) == 0 and num1 % (i + 1) == 0:
+                ans = i + 1
+    return (f'{num1} {num2}', str(ans))
+
+
+def main():
+    play_game(get_q_and_a)
+
+
+if __name__ == main:
+    main()
