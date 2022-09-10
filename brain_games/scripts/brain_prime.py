67a2e111629342bf774f4cc70f4de5b6e578eda6
onthemod <onthemod@yandex.ru>
2022-09-10 22:47:11 +0300

    added game prime numbers

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -5,24 +5,15 @@ from brain_games.game_engine import play_game
 
 
 def get_q_and_a():
-    num = random.randint(0, 100)
-    num = random.randint(0, length)
-    start = random.randint(0, 100)
-    step = random.randint(1, 10)
-    s = ''
-    ans = start
-    for i in range(length):
-        if i != num:
-            s = s + str(start) + ' '
-        else:
-            s = s + '.. '
-            ans = start
-        start = start + step
-    return (s, str(ans))
+    num = random.randint(0, 15)
+    for i in range(2, num):
+        if num % i == 0:
+            return (str(num), 'no')
+    return (str(num), 'yes')
 
 
 def main():
-    play_game(get_q_and_a)
+    play_game(get_q_and_a, 3, 'Answer "yes" if given number is prime. Otherwise answer "no".')
 
 
 if __name__ == main:
